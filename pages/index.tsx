b5ca5081b949984650c50f5fca9836fb93ad3ad2
Nathan Osti <nathanosti.job@gmail.com>
2022-10-14 14:40:17 -0300

    ✨ Finish base config for my template

--- pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1>Salve do Nathanzonho</h1>;
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <h1>Salve do Nathanzonho</h1>;
+};
+
+export default Home;
